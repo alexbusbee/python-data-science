4d8bc565adf368131cf2e8dc5b82be232097ef9f
Alex Busbee <AlexBusbee@mumacbook1733s-MacBook-Air.local>
2017-10-01 22:18:27 -0500

    Write to CSV

--- 2/demo.py
+++ 2/demo.py
@@ -1,5 +1,6 @@
 import tweepy
 from textblob import TextBlob
+import csv
 
 consumer_key = 'KoQL2MYFeSBC8nbgz1tcIc7Ci'
 consumer_secret = 'bRvp7l2jxg68kgLDqV2RKia3AHn0AOONZkU9egcmgvBbKv3ltG'
@@ -14,7 +15,15 @@ api = tweepy.API(auth)
 
 public_tweets = api.search('Trump')
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+with open('trump_tweets.csv', 'w') as csvfile:
+    fieldnames = ['Polarity', 'Sentiment', 'Tweet']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for tweet in public_tweets:
+        analysis = TextBlob(tweet.text)
+        if analysis.sentiment.polarity > 0:
+            writer.writerow({'Polarity': analysis.sentiment.polarity, 'Sentiment': 'Positive', 'Tweet': tweet.text})
+        else:
+            writer.writerow({'Polarity': analysis.sentiment.polarity, 'Sentiment': 'Negative', 'Tweet': tweet.text})
